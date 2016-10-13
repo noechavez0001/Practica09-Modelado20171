5dd5b1f00eaadac5abbfff4e444d6f27b8995821
Noe Chavez <noechavez0001@gmail.com>
2016-10-13 15:55:23 -0500

    funciona todo solo que no cambia la pocision de la serpiente

--- servidor.py
+++ servidor.py
@@ -4,71 +4,130 @@ import sys
 from PyQt4 import QtCore, QtGui, uic
 
 class servidorInterfaz(QtGui.QDialog):
-    def __init__(self):
-        QtGui.QDialog.__init__(self)
-        self.timer = QtCore.QTimer()
-        self.timer.timeout.connect(lambda: self.updateTable())
-        self.timer.start(250)
-        # Set up the user interface from Designer.
-        self.ui = uic.loadUi("servidor.ui")
-        self.ui.tableWidget.horizontalHeader().hide()
-        self.ui.tableWidget.verticalHeader().hide()
-        self.ui.tableWidget.setRowCount(20)
-        self.ui.tableWidget.setColumnCount(20)
-        ###self.ui.connect(self.ui.tableWidget, QtCore.SIGNAL(QtCore.keyPressEvent()), self, SLOT(self.keyPressEvent()))
-        self.ui.spin_filas.valueChanged.connect(lambda: self.resizeTable())
-        self.ui.spin_colum.valueChanged.connect(lambda: self.resizeTable())
-        self.ui.tableWidget.horizontalHeader().setResizeMode(QtGui.QHeaderView.Stretch)
-        self.ui.tableWidget.verticalHeader().setResizeMode(QtGui.QHeaderView.Stretch)
-        self.ui.spin_espera.valueChanged.connect(lambda: self.esperaAct())
-        self.ui.iniciar_juego.setCheckable(True)
-        self.ui.terminar_juego.setVisible(False)
-        self.ui.iniciar_juego.clicked.connect(lambda: self.ini_juego())
-        self.ui.terminar_juego.clicked.connect(lambda: self.ter_juego())
-        self.ui.show()
+	def __init__(self):
+		QtGui.QDialog.__init__(self)
+		self.timer = QtCore.QTimer()
+		self.direccion = "R"
+		self.snake = [(0,5),(0,4),(0,3),(0,2),(0,1),(0,0)]
+		self.items = []
+		self.timer.timeout.connect(lambda: self.updateTable())
+		self.timer.start(250)
+		# Set up the user interface from Designer.
+		self.ui = uic.loadUi("servidor.ui")
+		self.ui.tableWidget.horizontalHeader().hide()
+		self.ui.tableWidget.verticalHeader().hide()
+		self.ui.tableWidget.setRowCount(20)
+		self.ui.tableWidget.setColumnCount(20)
+		self.ui.tableWidget.keyPressEvent = self.keyPressEventTable
+		self.ui.spin_filas.valueChanged.connect(lambda: self.resizeTable())
+		self.ui.spin_colum.valueChanged.connect(lambda: self.resizeTable())
+		self.ui.tableWidget.horizontalHeader().setResizeMode(QtGui.QHeaderView.Stretch)
+		self.ui.tableWidget.verticalHeader().setResizeMode(QtGui.QHeaderView.Stretch)
+		self.ui.spin_espera.valueChanged.connect(lambda: self.esperaAct())
+		self.ui.iniciar_juego.setCheckable(True)
+		self.ui.terminar_juego.setVisible(False)
+		self.ui.iniciar_juego.clicked.connect(lambda: self.ini_juego())
+		self.ui.terminar_juego.clicked.connect(lambda: self.ter_juego())
+		self.ui.show()
 
-    def resizeTable(self):
-    	self.ui.tableWidget.setRowCount(self.ui.spin_filas.value())
-    	self.ui.tableWidget.setColumnCount(self.ui.spin_colum.value())
+	def resizeTable(self):
+		self.ui.tableWidget.setRowCount(self.ui.spin_filas.value())
+		self.ui.tableWidget.setColumnCount(self.ui.spin_colum.value())
 
-    def esperaAct(self):
-    	self.timer.start(self.ui.spin_espera.value())
+	def esperaAct(self):
+		self.timer.start(self.ui.spin_espera.value())
 
-    def updateTable(self):
-    	if (self.ui.iniciar_juego.isChecked()):
-    		#aqui va la actualizacion de la tabla
-    		print "hola"
+	def updateTable(self):
+		if (self.ui.iniciar_juego.isChecked()):
+			# aqui va la actualizacion de la tabla
+			if (self.direccion=="U"):
+				(x,y) = self.snake[0]
+				(a,b) = self.snake[-1]
+				c = (x + 1)% self.ui.spin_filas.value()
+				self.snake.insert(0,(c,y))
+				i = self.ui.tableWidget.itemAt(a,b)
+				self.ui.tableWidget.setItemSelected(i, False)
+				j = self.ui.tableWidget.itemAt(c,y)
+				self.ui.tableWidget.setItemSelected(j, True)
+				del self.snake[-1]
+			elif (self.direccion=="D"):
+				(x,y) = self.snake[0]
+				(a,b) = self.snake[-1]
+				c = (x - 1)% self.ui.spin_filas.value()
+				self.snake.insert(0,(c,y))
+				i = self.ui.tableWidget.itemAt(a,b)
+				self.ui.tableWidget.setItemSelected(i, False)
+				j = self.ui.tableWidget.itemAt(c,y)
+				self.ui.tableWidget.setItemSelected(j, True)
+				del self.snake[-1]
+			elif (self.direccion=="R"):
+				(x,y) = self.snake[0]
+				(a,b) = self.snake[-1]
+				c = (y + 1)% self.ui.spin_colum.value()
+				self.snake.insert(0,(x,c))
+				i = self.ui.tableWidget.itemAt(a,b)
+				self.ui.tableWidget.setItemSelected(i, False)
+				j = self.ui.tableWidget.itemAt(x,c)
+				self.ui.tableWidget.setItemSelected(j, True)
+				del self.snake[-1]
+			elif (self.direccion=="L"):
+				(x,y) = self.snake[0]
+				(a,b) = self.snake[-1]
+				c = (y - 1)% self.ui.spin_colum.value()
+				self.snake.insert(0,(x,c))
+				i = self.ui.tableWidget.itemAt(a,b)
+				self.ui.tableWidget.setItemSelected(i, False)
+				j = self.ui.tableWidget.itemAt(x,c)
+				self.ui.tableWidget.setItemSelected(j, True)
+				del self.snake[-1]
+			# colorear serpiente
+			if (self.snake[0] in self.snake[1:]):
+				self.ter_juego()
+			print self.snake
+
+	def ini_juego(self):
+		if (self.ui.iniciar_juego.isChecked()):
+			for x in range(len(self.snake)):
+				(a,b) = self.snake[x]
+				for x in range(self.ui.spin_filas.value()):
+					for y in range(self.ui.spin_colum.value()):
+						self.ui.tableWidget.setItem(a, b, QtGui.QTableWidgetItem("", 0))
+						self.ui.tableWidget.item(a,b).setBackground(QtGui.QColor(100,100,150))
+			self.ui.iniciar_juego.setText("Pausar Juego")
+			self.ui.terminar_juego.setVisible(True)
+			for x in range(len(self.snake)):
+				(a,b) = self.snake[x]
+				i = self.ui.tableWidget.itemAt(a,b)
+				self.ui.tableWidget.setItemSelected(i, True)
+			#aqui va la inicializacion de snake
+		else:
+			self.ui.iniciar_juego.setText("Iniciar Juego")
+
+	def ter_juego(self):
+		if (self.ui.iniciar_juego.isChecked()):
+			self.ui.tableWidget.clear()
+			self.ui.terminar_juego.setVisible(False)
+			self.ui.iniciar_juego.setCheckable(False)
+			self.ui.iniciar_juego.setCheckable(True)
+			self.ui.iniciar_juego.setText("Iniciar Juego")
+		self.ui.terminar_juego.setVisible(False)
+		#aqui va el reinicio de la tabla del juego
 
 
-    def ini_juego(self):
-    	if (self.ui.iniciar_juego.isChecked()):
-    		self.ui.iniciar_juego.setText("Pausar Juego")
-    		self.ui.terminar_juego.setVisible(True)
-    		#aqui va la inicializacion de snake
-    	else:
-    		self.ui.iniciar_juego.setText("Iniciar Juego")
-
-    def ter_juego(self):
-    	if (self.ui.iniciar_juego.isChecked()):
-    		self.ui.terminar_juego.setVisible(False)
-    		self.ui.iniciar_juego.setCheckable(False)
-    		self.ui.iniciar_juego.setCheckable(True)
-    		self.ui.iniciar_juego.setText("Iniciar Juego")
-    	self.ui.terminar_juego.setVisible(False)
-    	#aqui va el reinicio de la tabla del juego
-
-
-	def keyPressEvent(self, event):
-	    key = event.key()
-	    print(key)
-	    if key == QtCore.Qt.Key_Left:
-	        print('Left Arrow Pressed')
-	    elif key == QtCore.Qt.Key_Up:
-	    	print('Up Arrow Pressed')
-	    elif key == QtCore.Qt.Key_Right:
-	    	print('Right Arrow Pressed')
-	    elif key == QtCore.Qt-Key_Down:
-    		print('Down Arrow Pressed')
+	def keyPressEventTable(self, event):
+		key = event.key()
+		if key == QtCore.Qt.Key_Left:
+			self.direccion = "L"
+			print('Left Arrow Pressed')
+		elif key == QtCore.Qt.Key_Up:
+			self.direccion = "U"
+			print('Up Arrow Pressed')
+		elif key == QtCore.Qt.Key_Right:
+			self.direccion = "R"
+			print('Right Arrow Pressed')
+		elif key == QtCore.Qt.Key_Down:
+			self.direccion = "D"
+			print('Down Arrow Pressed')
 
 app = QtGui.QApplication(sys.argv)
 window = servidorInterfaz()
